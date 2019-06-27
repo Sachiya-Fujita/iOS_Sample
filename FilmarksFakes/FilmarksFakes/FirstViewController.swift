02acc6403b8975df64b0e6238ee5c1dbacebde91
Sachiya-Fujita <s4ch1mos@gmail.com>
2019-06-27 13:05:43 +0900

    Projectの作成

---
+++ FilmarksFakes/FilmarksFakes/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FilmarksFakes
+//
+//  Created by 1st-impact on 2019/06/27.
+//  Copyright © 2019 S4ch1mos. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FilmarksFakes/FilmarksFakes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FilmarksFakes
+//
+//  Created by 1st-impact on 2019/06/27.
+//  Copyright © 2019 S4ch1mos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
